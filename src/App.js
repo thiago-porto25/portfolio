580d7bdf6d2d7e6d278983d9e789eab02b5457d9
Thiago Porto <thiago.porto@d3.do>
2022-01-15 14:08:31 -0300

    rm some boilerplate

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header text-lg uppercase">
+      <header className="text-lg uppercase App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
