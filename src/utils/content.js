e6d56a99ff3ddafef3e6d44cb25e25dce36b7cfc
Thiago Porto <thiago.porto@d3.do>
2022-01-18 22:45:32 -0300

    create content util

---
+++ src/utils/content.js
@@ -0,0 +1,11 @@
+export const content = {
+  intro: {},
+  info: {},
+  projects: {},
+  social: {},
+  footer: {
+    text: "© Thiago Porto. All Rights Reserved.",
+    linkText: "thiagoportodev@gmail.com",
+    link: "mailto:thiagoportodev@gmail.com",
+  },
+};
