e1d9ead0e62ed647b7978026511fb642b95ec35f
Thiago Porto <thiago.porto@d3.do>
2022-01-23 15:00:25 -0300

    implement basic ThreeJS

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,30 +8,67 @@ import { content } from "../utils/content";
 
 export default function Home() {
   useEffect(() => {
+    // Starting Three JS
     const renderer = new THREE.WebGLRenderer({
       canvas: document.querySelector("canvas"),
+      antialias: true,
+      alpha: true,
     });
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setPixelRatio(window.devicePixelRatio);
-
     const scene = new THREE.Scene();
+
+    // Camera Setup
     const camera = new THREE.PerspectiveCamera(
-      75,
+      50,
       window.innerWidth / window.innerHeight,
       0.1,
       1000
     );
+    camera.position.z = 50;
 
-    const geometry = new THREE.TorusGeometry(10, 3, 16, 30);
-    const material = new THREE.MeshBasicMaterial({ color: 0x323233 });
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
-    camera.position.z = 5;
+    // Lights
+    const pointLight = new THREE.PointLight(0x555555, 1);
+    pointLight.position.set(15, 15, 15);
+
+    const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+
+    scene.add(ambientLight, pointLight);
+
+    // Triangle
+    const geometry = new THREE.TorusGeometry(7, 2, 5, 5);
+    const material = new THREE.MeshStandardMaterial({
+      color: 0xffffff,
+      metalness: 1,
+      roughness: 0.6,
+    });
+    const triangle = new THREE.Mesh(geometry, material);
+    scene.add(triangle);
+
+    // Functions
+    const addStar = () => {
+      const starGeometry = new THREE.SphereGeometry(0.1, 15, 15);
+      const startMaterial = new THREE.MeshBasicMaterial({ color: 0xeeeeee });
+
+      const star = new THREE.Mesh(starGeometry, startMaterial);
+
+      const [x, y, z] = Array(3)
+        .fill()
+        .map(() => THREE.MathUtils.randFloatSpread(100));
+
+      star.position.set(x, y, z);
+      scene.add(star);
+    };
+
+    Array(75).fill().forEach(addStar);
 
     const animate = function () {
       requestAnimationFrame(animate);
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
+
+      triangle.rotation.x += 0.008;
+      triangle.rotation.y += 0.009;
+      triangle.rotation.z += 0.006;
+
       renderer.render(scene, camera);
     };
 
@@ -42,13 +79,13 @@ export default function Home() {
     <>
       <canvas />
 
-      <main>
+      <main className="absolute z-10">
         <Intro content={content.intro} />
         <Info content={content.info} />
         <Projects content={content.projects} />
         <SocialNetworks content={content.social} />
+        <Footer content={content.footer} />
       </main>
-      <Footer content={content.footer} />
     </>
   );
 }
